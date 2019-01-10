136baa25574fa8d0829fb7d8c728feee1b3eaa73
Mir Islam <mislam00@stuy.edu>
2019-01-10 15:35:42 -0500

    testing out if the constructor works

--- ImageFilters.java
+++ ImageFilters.java
@@ -2,27 +2,27 @@ import java.awt.Image;
 import java.awt.image.BufferedImage;
 public class ImageFilters{
   private int[][] colorArray;
-  public File imgFile;
+  public String imgFile;
 
   ImageFilters(){
-    File imgFile = new File(imgFile);
-    for(int i = 0; i < imgFile.getwidth(); i++){
-      for(int z = 0; z < imgFile.getheight(); z++){
-          colorArray[i][h] = imageFile(imgFile.getRGB(i, z));
-      }
-    }
+    File imgFile = new ImageIO.read(new File(imgFile));
+    //for(int i = 0; i < imgFile.getwidth(); i++){
+      //for(int z = 0; z < imgFile.getheight(); z++){
+          //colorArray[i][h] = imageFile(imgFile.getRGB(i, z));
+      //}
+    //}
   }
 
-  public void grayscale(int[] pixels){
-
+  public int grayscale(int[] pixels){
+    return 0;
   }
 
-  public void blur(int[] pixels){
-
+  public int blur(int[] pixels){
+    return 0;
   }
 
-  public void sharpen(int[] pixels){
-    
+  public int sharpen(int[] pixels){
+    return 0;
   }
 
 }
