9a26541991f20c1d4ac7de92bf2a116e33a2571f
mirislam486 <34451472+mirislam486@users.noreply.github.com>
2019-01-09 23:44:49 -0500

    Add files via upload

--- ImageFilters.java
+++ ImageFilters.java
@@ -1,5 +1,15 @@
 import java.awt.Image;
 import java.awt.image.BufferedImage;
 public class ImageFilters{
+  private int[][] colorArray;
+  public File imgFile;
 
+  ImageFilters(){
+    File imgFile = new File(imgFile);
+    for(int i = 0; i < imgFile.getwidth(); i++){
+      for(int z = 0; z < imgFile.getheight(); z++){
+          colorArray[i][h] = imageFile(imgFile.getRGB(i, z));
+      }
+    }
+  }
 }
