47fb9f3ba5ece801642a0a19b97318f86217dfed
Mir Islam <mislam00@stuy.edu>
2019-01-13 01:25:24 -0500

    still working on files

--- ImageFilters.java
+++ ImageFilters.java
@@ -1,13 +1,11 @@
 import java.awt.Image;
 import java.awt.image.BufferedImage;
-import javax.imageio;
 public class ImageFilters{
   private int[][] colorArray;
   public String imgFile;
 
-  public ImageFilters(){
-    File imgFile = new File(fileName);
-    BufferedImage image = new ImageIO.read(new File(imgFile));
+  ImageFilters(){
+    File imgFile = new ImageIO.read(new File(imgFile));
     //for(int i = 0; i < imgFile.getwidth(); i++){
       //for(int z = 0; z < imgFile.getheight(); z++){
           //colorArray[i][h] = imageFile(imgFile.getRGB(i, z));
@@ -25,10 +23,6 @@ public class ImageFilters{
 
   public int sharpen(int[] pixels){
     return 0;
-  }
-
-  public static void main(String[] args){
-    System.out.println(imgFile);
   }
 
 }
