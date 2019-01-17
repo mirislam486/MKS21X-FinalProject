c7ef9d2987bf3d51d95a89e1b50b24114e81a943
Mir Islam <mislam00@stuy.edu>
2019-01-16 21:33:58 -0500

    Got it to return values but it's negative

--- ImageFilters.java
+++ ImageFilters.java
@@ -3,21 +3,24 @@ import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 import java.io.*;
 import java.awt.image.DataBufferByte;
+import java.util.*;
 public class ImageFilters{
   public static int[][] colorArray;
   public static BufferedImage ogImage;
   public static String imgFile;
+  public static int height;
+  public static int width;
 
-  public ImageFilters(String imgFile){
-    int width = ogImage.getWidth();
-    int height = ogImage.getHeight();
-    colorArray = new int[height][width];
+  public ImageFilters(String imgFile)throws IOException{
 
-    for (int ycor = 0; ycor < height; ycor++) {
-       for (int xcor = 0; xcor < width; xcor++) {
-          colorArray[ycor][xcor] = ogImage.getRGB(xcor, ycor);
-       }
-    }
+    //File original = new File("cake.jpg");
+    //ogImage = ImageIO.read(original);
+
+    //for (int ycor = 0; ycor < height; ycor++){
+      // for (int xcor = 0; xcor < width; xcor++) {
+        //  colorArray[ycor][xcor] = ogImage.getRGB(xcor, ycor);
+    //  }
+    //}
   }
 
 //  public static void openFile(String imgFile) throws IOException{
@@ -39,13 +42,23 @@ public class ImageFilters{
   //      return colorArray;
   //   }
 
-  public int[][] grayscale(int ycor, int xcor){
-    for(int x = 0; x < ogImage.getWidth(); x++){
-      for(int y = 0; y < ogImage.getHeight(); y++){
-        colorArray[x][y] = 404040;
+  public static void setStuff(){
+    width = ogImage.getWidth();
+    height = ogImage.getHeight();
+  }
+
+  public static String toString(int input){
+    return input + "";
+  }
+
+  public static ArrayList<String> grayscale(){
+    ArrayList<String> pixels = new ArrayList<String>();
+    for(int x = 0; x < width; x++){
+      for(int y = 0; y < height; y++){
+         pixels.add(toString(ogImage.getRGB(x, y)));
       }
     }
-    return colorArray;
+    return pixels;
   }
 
   public int blur(int[] pixels){
@@ -58,6 +71,8 @@ public class ImageFilters{
 
   public static void main(String[] args) throws IOException{
     File original = new File("cake.jpg");
-    System.out.println(colorArray);
+    ogImage = ImageIO.read(original);
+    System.out.println(height);
+
   }
 }
