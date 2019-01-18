8abc70deb5cddac4fcc8172d81ad19741baf0e80
Mir Islam <mislam00@stuy.edu>
2019-01-17 23:36:24 -0500

    How do I fix this D:

--- ImageFilters.java
+++ ImageFilters.java
@@ -5,42 +5,25 @@ import java.io.*;
 import java.awt.image.DataBufferByte;
 import java.util.*;
 public class ImageFilters{
-  public static int[][] colorArray;
+  public static ArrayList<String> pixels;
   public static BufferedImage ogImage;
   public static String imgFile;
   public static int height;
   public static int width;
 
   public ImageFilters(String imgFile)throws IOException{
-
-    //File original = new File("cake.jpg");
-    //ogImage = ImageIO.read(original);
-
-    //for (int ycor = 0; ycor < height; ycor++){
-      // for (int xcor = 0; xcor < width; xcor++) {
-        //  colorArray[ycor][xcor] = ogImage.getRGB(xcor, ycor);
-    //  }
-    //}
+    File original = new File(imgFile);
+    ogImage = ImageIO.read(original);
+    ArrayList<String> pixels = new ArrayList<String>();
+    width = ogImage.getWidth();
+    height = ogImage.getHeight();
+    for(int x = 0; x < width; x++){
+      for(int y = 0; y < height; y++){
+         pixels.add(toString(ogImage.getRGB(x, y)));
+       }
+     }
   }
 
-//  public static void openFile(String imgFile) throws IOException{
-//      File original = new File(imgFile);
-//      ogImage = ImageIO.read(original);
-//  }
-
-//  private static int[][] getPixels(){
-//        int width = ogImage.getWidth();
-//        int height = ogImage.getHeight();
-//        colorArray = new int[height][width];
-
-//        for (int ycor = 0; ycor < height; ycor++) {
-//           for (int xcor = 0; xcor < width; xcor++) {
-//              colorArray[ycor][xcor] = ogImage.getRGB(xcor, ycor);
-  //         }
-  //      }
-
-  //      return colorArray;
-  //   }
 
   public static void setStuff(){
     width = ogImage.getWidth();
@@ -51,18 +34,19 @@ public class ImageFilters{
     return input + "";
   }
 
-  public static ArrayList<String> grayscale(){
+  public static void pixelArray(){
     ArrayList<String> pixels = new ArrayList<String>();
+    width = ogImage.getWidth();
+    height = ogImage.getHeight();
     for(int x = 0; x < width; x++){
       for(int y = 0; y < height; y++){
          pixels.add(toString(ogImage.getRGB(x, y)));
       }
     }
-    return pixels;
   }
 
-  public int blur(int[] pixels){
-    return 0;
+  public static ArrayList<String> grayscale(){
+    return pixels;
   }
 
   public int sharpen(int[] pixels){
@@ -70,9 +54,8 @@ public class ImageFilters{
   }
 
   public static void main(String[] args) throws IOException{
-    File original = new File("cake.jpg");
-    ogImage = ImageIO.read(original);
-    System.out.println(height);
+    ImageFilters newFile = new ImageFilters("cake.jpg");
+    System.out.println(newFile.grayscale());
 
   }
 }
